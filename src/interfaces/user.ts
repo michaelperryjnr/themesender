12d154ec0ecc78484519294e44127700acfe3002
michaelperryjnr <michaelperryt97@gmail.com>
2024-05-13 10:28:20 +0000

    App interfaces

---
+++ src/interfaces/user.ts
@@ -0,0 +1,13 @@
+export default interface User {
+  isUser: true;
+  isGroup: false;
+  uid: string;
+  displayName: string;
+  email: string;
+  photoURL: string;
+  bio: string;
+  location: string;
+  status: string;
+  contacts: string[];
+  lastSeen?: any;
+}
